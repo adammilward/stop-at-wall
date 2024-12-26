e9c2d83857da4328df90b8257358490679056bb9
Adam Milward <2379055+adammilward@users.noreply.github.com>
2024-12-26 23:44:45 +0000

    improve noise

--- main.ts
+++ main.ts
@@ -4,7 +4,8 @@ basic.forever(function () {
     if (Sonar > 2 && Sonar < 15) {
         cuteBot.motors(0, 0)
         for (let index = 0; index < 4; index++) {
-            music.play(music.tonePlayable(880, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+            music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+            music.play(music.tonePlayable(659, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
         }
         basic.pause(2000)
         cuteBot.motors(randint(-50, -100), 0)
